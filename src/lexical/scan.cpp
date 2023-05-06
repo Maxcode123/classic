5d07e2ec11bfdf4f30203936d2d1053e19925b9f
Maximos Nikiforakis <nikiforos@live.co.uk>
2023-05-06 18:50:01 +0300

    scan print

--- src/lexical/scan.cpp
+++ src/lexical/scan.cpp
@@ -14,7 +14,7 @@ int main(int argc, char **argv) {
     for (;;) {
         i = yylex();
         if (i == 0) break;
-        std::cout << yylval.lexeme_str << " " << pos << "\n";
+        std::cout << yylval.lexeme_str << " " << pos << " " << yylval.semantval() << "\n";
     }
     return 0;
 }
